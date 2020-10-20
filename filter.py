5993ad55d17fb0bb15b64852de19fc6e063beba6
AKAZH ID <36891477+justakazh@users.noreply.github.com>
2020-10-20 18:09:39 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,5 +1,8 @@
 listn = [i.strip() for i in open(raw_input("input list >>"), "r").readlines()]
 print("[+] Filtering . . . ")
 for i in listn:
-    x = i.split("/")
-    open("filtered.txt", "a").write("http://"+x[2]+"\n")
+    try:
+        x = i.split("/")
+        open("filtered.txt", "a").write("http://"+x[2]+"\n")
+    except:
+        pass
